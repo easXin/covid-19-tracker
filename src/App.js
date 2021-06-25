86f803047550b31ebc010807a4e3daa278676ba4
easxin <codingericwang@gmail.com>
2021-06-25 11:53:04 -0400

    Organizes structure of covid 19 tracker app, added app design wireframe under the root folder

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ function App() {
   return (
     <div className="app">
       <p>Building Covid19 app</p>
+      {/* Header */}
+      {/* Title Select input dropdown filed */}
+
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+
+      {/* Table */}
+      {/* Graph */}
+      
+      {/* Map */}
     </div>
   );
 }
