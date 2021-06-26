5ac2fc4b0ecfae92af0b1e0213b4baf7ef075b69
easxin <codingericwang@gmail.com>
2021-06-26 12:42:06 -0400

    make an api call and fetch the data from disease.sh, and populate on react front end page "

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import FormControl from '@material-ui/core/FormControl'
 import Select from '@material-ui/core/Select'
-import ManuItem from '@material-ui/core/MenuItem'
+import MenuItem from '@material-ui/core/MenuItem'
 import './App.css';
 
 function App() {
-  const [countries, setCountries] = useState(["US", "CN", "Euro"])
+  const [countries, setCountries] = useState([])
+  const [country, setCountry] = useState('worldwide')
+
+  // async : I want to send a request, wait for it , do something with it 
+  // await :  do something with promise
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        // json() takes in a res, return a promise
+        .then(response => response.json())
+        .then(data => {
+          const countries = data.map((country) => (
+            {
+              name: country.country, // United States
+              value: country.countryInfo.iso2 // USA
+            }
+          ))
+          setCountries(countries)
+        })
+    }
+    getCountriesData()
+    /*
+      Step of api call
+      1. use async to make api call
+      2. turn it into json format
+      3. parse the data, and filter out the needed one
+      4. call api function itself
+      5. populate the data on front end page
+    */
+  }, [])
+
+  // []  only run one time
+  // [xxx]  re-execute if xxx change 
+
+  const handleCountryChange = async (event) => {
+    //console.log(event)
+    const countryCode = event.target.value
+    //console.log("1000 >>>", countryCode)
+    setCountry(countryCode)
+  }
   return (
     <div className="app">
-      {/* useState  setup variables in react*/}
       <div className="app__header">
         {/* Header */}
         <p>Covid-19 Tracker</p>
@@ -16,11 +54,13 @@ function App() {
         <FormControl className="app__dropdown">
           <Select
             variant="outlined"
-            value="abc"
+            value={country} //??
+            onChange={handleCountryChange}
           >
+            <MenuItem value="worldwide">Worldwide</MenuItem>
             {
               countries.map((country) => (
-                <ManuItem value={country}>{country}</ManuItem>
+                <MenuItem value={country.value}>{country.name}</MenuItem>
               ))
             }
 
