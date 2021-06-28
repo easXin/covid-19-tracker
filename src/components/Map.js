04951df350f23632cf088a306dbfb5d3b58fa805
easxin <codingericwang@gmail.com>
2021-06-28 11:56:02 -0400

    add some code

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Map as LeafletMap, TileLayer } from "react-leaflet"
+//import { Map as M, TileLayer, Marker, Popup } from 'react-leaflet'
 import { showDataOnMap } from "../utils/utils";
 import "./Map.css";
 
@@ -19,4 +20,25 @@ function Map({ countries, casesType, center, zoom }) {
     )
 }
 
+// function Map({ countries, casesType, center, zoom }) {
+//     const position = [51.505, -0.09]
+//     return (
+
+//         <div className="map">
+//             {console.log("Yolo")}
+//             <M center={position} zoom={13} scrollWheelZoom={false} >
+//                 <TileLayer
+//                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//                 />
+//                 <Marker position={position}>
+//                     <Popup>
+//                         A pretty CSS3 popup. <br /> Easily customizable.
+//                     </Popup>
+//                 </Marker>
+//             </M>
+//         </div>
+
+//     )
+// }
 export default Map
