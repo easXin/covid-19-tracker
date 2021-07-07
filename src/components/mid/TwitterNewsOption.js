a308631d0db4bc5958e450f5ef31d6b6a6bf16d7
easxin <codingericwang@gmail.com>
2021-07-06 22:09:35 -0400

    it's time for dinner

--- src/components/mid/TwitterNewsOption.js
+++ src/components/mid/TwitterNewsOption.js
@@ -2,15 +2,23 @@ import React from 'react'
 import './TwitterNewsOption.css'
 
 const checkMark = "https://i.kym-cdn.com/photos/images/newsfeed/001/476/144/030.jpg"
-
+const twitterLogo = "https://image.flaticon.com/icons/png/512/733/733579.png"
 function TwitterNewsOption({ Icon, SrcFrom, twitterId, twitterName }) {
     return (
         <div className="twitterNewsOption">
-            <p>Icon</p>
-            <p>TwitterName</p>
-            <p>TwitterId</p>
-            <p>Twitter Logo</p>
+            <div className="twitterNewsOptionLeft">
+                <Icon />
+                <div className="twitterNewsOptionLeftInfo">
+                    <h4>TwitterName
+                        <img src={checkMark} alt="check marker" />
+                    </h4>
+                    <p>@TwitterId</p>
+                </div>
 
+            </div>
+            <div className="twitterNewsOptionRight">
+                <img src={twitterLogo} alt="twitter logo" />
+            </div>
         </div>
     )
 }
